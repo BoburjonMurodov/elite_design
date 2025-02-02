44b394cdab707ce517de163a5e7256ab95ce3791
Boburjon Murodov <boburjonmurodov06@gmail.com>
2025-02-02 15:20:14 +0500

    Fix GitHub Actions workflow

---
+++ lib/features/main/presentation/bloc/cart/cart_screen_event.dart
@@ -0,0 +1,4 @@
+part of 'cart_screen_bloc.dart';
+
+@immutable
+sealed class CartScreenEvent {}
---
+++ lib/features/main/presentation/bloc/cart/cart_screen_state.dart
@@ -0,0 +1,6 @@
+part of 'cart_screen_bloc.dart';
+
+@immutable
+sealed class CartScreenState {}
+
+final class CartScreenInitial extends CartScreenState {}
